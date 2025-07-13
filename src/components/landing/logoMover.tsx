4abe182f54edabc3a5c4d39e94fe56a099ddfe1a
JagdishSuthar1 <jagdishsuthar4581@gmail.com>
2025-07-13 23:48:56 +0530

    change company line

--- src/components/landing/logoMover.tsx
+++ src/components/landing/logoMover.tsx
@@ -4,7 +4,7 @@ export default function LogoMover() {
 return (
     
     <div className="w-full overflow-hidden">
-    <motion.div className="flex  min-w-max  h-30 py-9 [mask-image:linear-gradient(to_right,transparent,black,transparent)]" animate ={{translateX : '-50%'}}  transition={{duration : 30 , repeat : Infinity , repeatType : 'loop', ease : "linear"}}>
+    <motion.div className="flex  min-w-max  h-30 py-9 [mask-image:linear-gradient(to_right,transparent,black,transparent)] text-5xl font-bold" animate ={{translateX : '-50%'}}  transition={{duration : 30 , repeat : Infinity , repeatType : 'loop', ease : "linear"}}>
         {/* <img src="/logo-acme.png" alt="" className="h-13 object-contain" />
         <img src="/logo-apex.png" alt=""  className="h-13 object-contain"/>
         <img src="/logo-pulse.png" alt="" className="h-13 object-contain" />
